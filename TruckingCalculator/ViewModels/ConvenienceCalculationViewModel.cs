583f5fd324980002016e2f55f36d8db4df3b5961
Fallen <disgaea@outlook.com>
2018-10-18 22:30:54 -0400

    Added non-functional ConvenienceCalculationPage Updated AboutPage

---
+++ TruckingCalculator/ViewModels/ConvenienceCalculationViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace TruckingCalculator.ViewModels
+{
+	public class ConvenienceCalculationViewModel : BaseViewModel
+    {
+        public decimal OriginalPayment
+        {
+            get;
+            set;
+        }
+
+        public decimal FinalPayment
+        {
+            get;
+            set;
+        }
+
+        public decimal PercentageOfMoneyWitheld
+        {
+            get;
+            set;
+        }
+        public ConvenienceCalculationViewModel()
+        {
+        }
+    }
+}
--- TruckingCalculator/Views/MenuPage.xaml.cs
+++ TruckingCalculator/Views/MenuPage.xaml.cs
@@ -19,7 +19,8 @@ namespace TruckingCalculator.Views
             menuItems = new List<HomeMenuItem>
             {
                 new HomeMenuItem {Id = MenuItemType.Browse, Title="Browse" },
-                new HomeMenuItem {Id = MenuItemType.About, Title="About" }
+                new HomeMenuItem {Id = MenuItemType.About, Title="About" },
+                new HomeMenuItem {Id = MenuItemType.ConvenienceCalculation, Title="Convenience Calculation" }
             };
 
             ListViewMenu.ItemsSource = menuItems;
